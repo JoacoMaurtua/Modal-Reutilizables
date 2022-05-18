e2b8f24dedc2a09a56f4b8f6bbe7d79a63a1cc65
JoacoMaurtua <joacomaurtua@gmail.com>
2022-05-18 13:12:08 -0500

    funcionalidad terminada

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from './components/Modal/Modal.js';
 import './App.scss';
 
 function App() {
+  const [estadoModal1, cambiarEstadoModal1] = useState(false);
+
   return (
     <>
       <div className="contenedor-botones">
-        <button className="boton">Modal 1</button>
+        <button
+          className="boton"
+          onClick={() => cambiarEstadoModal1(!estadoModal1)}
+        >
+          Modal 1
+        </button>
       </div>
-      <Modal>
+      <Modal estado={estadoModal1} cambiarEstado={cambiarEstadoModal1}>
         <div className="contenido">
           <h1>Ventana Modal</h1>
           <p>Reutilizable y con opciones de personalizaciones</p>
-          <button className="boton">Aceptar</button>
+          <button
+            className="boton"
+            onClick={() => cambiarEstadoModal1(!estadoModal1)}
+          >
+            Aceptar
+          </button>
         </div>
       </Modal>
     </>
--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import './Modal.scss';
 
-const Modal = ({children}) => {
+const Modal = ({children, estado, cambiarEstado}) => {
   return (
     <>
-      <div className="overlay">
-        <div className="contenedor-modal">
-          <div className="encabezado-modal">
-            <h3>Titulo</h3>
+      {estado &&
+        <div className="overlay">
+          <div className="contenedor-modal">
+            <div className="encabezado-modal">
+              <h3>Titulo</h3>
+            </div>
+
+            <button 
+              className="btn-cerrar"
+              onClick={()=>cambiarEstado(false)}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+              </svg>
+            </button>
+            {children}
           </div>
 
-          <button className="btn-cerrar">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-            </svg>
-          </button>
-          {children}
         </div>
-
-      </div>
+      }
     </>
   )
 }
