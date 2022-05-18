db540e02d663bdb95beafc41afeed0dc879d8552
JoacoMaurtua <joacomaurtua@gmail.com>
2022-05-18 11:28:54 -0500

    estructura del modal y estilos

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Modal from './components/Modal/Modal.js';
 import './App.scss';
 
 function App() {
   return (
-    <div className="contenedor-botones">
-      <button>Modal 1</button>
-    </div>
+    <>
+      <div className="contenedor-botones">
+        <button>Modal 1</button>
+      </div>
+      <Modal/>
+    </>
   );
 }
 
---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Modal.scss';
+
+const Modal = () => {
+  return (
+    <>
+      <div className="overlay">
+        <div className="contenedor-modal">
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Modal
